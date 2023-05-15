4fcbd2e0d5e619366cee1285d00d78abe69092b2
lazuardi <lazuardi.reznnov@gmail.com>
2023-05-15 16:00:49 +0700

    Stock supplier Form

---
+++ resources/views/dashboard/stock/supplier/create.blade.php
@@ -0,0 +1,21 @@
+<x-dashboard>
+    <x-pagetitle title="{{ $title }}">
+        <x-breadcrumb>
+            <x-breadcrumb-item link="/dashboard" name="Dashboard" />
+            <x-breadcrumb-item link="/dashboard/stock" name="Stock" />
+            <x-breadcrumb-item
+                link="/dashboard/stock/supplier"
+                name="Supplier"
+            />
+            <x-breadcrumb-item link="" name="Form " />
+        </x-breadcrumb>
+    </x-pagetitle>
+
+    <div class="row">
+        <div class="col-md-8">
+            <x-card>
+                <x-card-title> Form Supplier </x-card-title>
+            </x-card>
+        </div>
+    </div>
+</x-dashboard>

e499c93ccdb4d1c10d6e84d87847e1717cea572f
lazuardi <lazuardi.reznnov@gmail.com>
2023-08-07 15:08:41 +0700

    Update Minor

--- resources/views/dashboard/maintenance/show2.blade.php
+++ resources/views/dashboard/maintenance/show2.blade.php
@@ -395,12 +395,83 @@
                                 class="tab-pane fade progres pt-3"
                                 id="progres"
                             >
-                                @push('css')
-                                <livewire:styles />
-                                @endpush @push('script')
-                                <livewire:scripts />
-                                @endpush @livewire('maintenance.progress.table',
-                                ['maintenanceId'=>$data->id])
+                                <div class="row my-3">
+                                    <div class="col">
+                                        <div class="progress">
+                                            <div
+                                                class="progress-bar"
+                                                role="progressbar"
+                                                style="width: {{ $data->progress }}%"
+                                                aria-valuenow="25"
+                                                aria-valuemin="0"
+                                                aria-valuemax="100"
+                                            >
+                                                {{ $data->progress }}%
+                                            </div>
+                                        </div>
+                                        <ul class="list-group my-3">
+                                            @foreach($data->statelog as $sl)
+                                            <li
+                                                class="list-group-item d-flex justify-content-between"
+                                            >
+                                                <div>
+                                                    {{ $sl->description }}
+                                                    <br />
+                                                    <small
+                                                        class="text-muted"
+                                                        >{{ $sl->updated_at->diffForHumans() }}</small
+                                                    >
+                                                </div>
+                                                <div>
+                                                    <a
+                                                        href="/dashboard/maintenance/logstate/{{ $data->slug }}/{{ $sl->id }}/edit"
+                                                        class="badge bg-warning"
+                                                        data-bs-toggle="tooltip"
+                                                        data-bs-placement="top"
+                                                        title="Progress maintenance"
+                                                    >
+                                                        <i
+                                                            class="bi bi-pencil-square"
+                                                        ></i>
+                                                    </a>
+                                                    <form
+                                                        action="/dashboard/maintenance/logstate/{{ $data->slug }}"
+                                                        method="post"
+                                                        class="d-inline"
+                                                    >
+                                                        @method('delete') @csrf
+                                                        <input
+                                                            type="hidden"
+                                                            name="id"
+                                                            value="{{ $sl->id }}"
+                                                        />
+                                                        <button
+                                                            class="badge bg-danger border-0"
+                                                            data-bs-toggle="tooltip"
+                                                            data-bs-placement="top"
+                                                            title="Delete maintenance"
+                                                            onclick="return confirm('are You sure ??')"
+                                                        >
+                                                            <i
+                                                                class="bi bi-x-lg"
+                                                            ></i>
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </li>
+                                            @endforeach
+                                        </ul>
+                                        <x-button-link
+                                            href="/dashboard/maintenance/logstate/{{ $data->slug }}"
+                                            class="btn-success"
+                                            data-bs-toggle="tooltip"
+                                            data-bs-placement="top"
+                                            title="Progress maintenance"
+                                        >
+                                            Update Progress
+                                        </x-button-link>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </x-card-body>
